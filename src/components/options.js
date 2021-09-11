f6032437bce4eb36612102403717db71201fd12c
raunaqsingh2020 <raunaqsingh2020@gmail.com>
2021-09-11 13:40:09 -0400

    toggle button

--- src/components/options.js
+++ src/components/options.js
@@ -4,24 +4,21 @@ import styled from 'styled-components'
 import { POPPINS_REGULAR } from '../styles/font'
 
 const SectionWrapper = styled.div`
-  //display: flex;
-  //align-items: center; 
   margin: 0;
   padding: 0;
-//   background-color: #B6BDFF;
-//   height: 200px;
-//   width: 200px;
 `
 
 const OptionWrapper = styled.div`
   display: flex;
+  align-items: center; 
+  padding: 0 1.3rem 0 1.3rem;
 `
 
 const OptionText = styled.h1`
   font-size: 15px;
   color: #1B1B1B;
   margin-right: auto;
-  margin-left: 10px;
+  // margin-left: 10px;
   ${POPPINS_REGULAR}
 `
 
@@ -31,7 +28,7 @@ const Options = () => {
           <OptionWrapper>
               <OptionText>Unrealistic Female Body Standards</OptionText>
               <label class="switch">
-                <input type="checkbox" checked/>
+                <input type="checkbox"/>
                 <span class="slider round"></span>
               </label>
           </OptionWrapper>
